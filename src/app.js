00139d8d8e56bf03d4b813d8d0e8fdeb38795f3b
LeandroMatonte <leandromatonte99@gmail.com>
2021-04-16 12:55:40 +0000

    src/app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+var who = ["The dog", "My grandma", "His turtle", "My bird"];
+var action = ["ate", "peed", "crushed", "broke"];
+var what = ["my homework", "the keys", "the car"];
+var when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function concatenar(who, action, what, when) {
+  var randomWho = who[Math.floor(Math.random() * 4)];
+  var randomAction = action[Math.floor(Math.random() * 4)];
+  var randomWhat = what[Math.floor(Math.random() * 4)];
+  var randomWhen = when[Math.floor(Math.random() * 4)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  return (document.querySelector("#excuse").innerHTML =
+    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen);
+}
+
+window.onload = () => {
+  concatenar(who, action, what, when);
 };
